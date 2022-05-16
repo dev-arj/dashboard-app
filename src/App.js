f6e0ea43c51e69053d84f5788b58fac2b0e5df74
Arjun <rockztar003@gmail.com>
2022-05-16 11:43:19 +0530

    added Logo, Header and Footer

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <header>DashBoard</header>
       <div className="NavBar">
+        <div className='Logo'>
+          <h1>DB</h1>
+        </div>
+        <div className="Tabs">
         <div className="Home">Home</div>
         <div className="Policy">Policy</div>
         <div className="About">About</div>
         <div className="ContactUs">Contact Us</div>
+        </div>
       </div>
       <div className="Sector-2">
-        <div className="SideMenu">SideMenu</div>
-        <div className="PresentationWindow">PresentationWindow</div>
+        <div className="SideMenu">
+          <h1 className="SideMenu-Heading">SideMenu</h1>
+        </div>
+        <div className="PresentationWindow">
+          <h1 className="PresentationWindow-Heading">PresentationWindow</h1>
+        </div>
       </div>
+      <footer>Copyright issued</footer>
     </div>
   );
 }
