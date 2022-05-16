b078729fa9a1da1f1722d44b574f8fb840704232
Arjun <rockztar003@gmail.com>
2022-05-16 11:09:59 +0530

    First commit - Initial files added

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="NavBar">
+        <div className="Home">Home</div>
+        <div className="Policy">Policy</div>
+        <div className="About">About</div>
+        <div className="ContactUs">Contact Us</div>
+      </div>
+      <div className="Sector-2">
+        <div className="SideMenu">SideMenu</div>
+        <div className="PresentationWindow">PresentationWindow</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
